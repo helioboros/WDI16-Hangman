c9dc4963fd1bcf0eed5118cf35cd8678f53dd982
helioboros <felinewraith@gmail.com>
2018-05-21 11:22:05 -0400

    bare bones pseudocode

---
+++ script.js
@@ -0,0 +1,24 @@
+// big ole hangman object array:
+//
+// function for resetting
+//      - Lives: 6
+//      - Word: random from such and such array
+//      - Word display: updated
+//      - Letter display: filled out
+//
+// object for stuff having to do with lives?
+//      function remove 1 life
+//      Lives is an array? Or a number? Think a number
+//      Display: some kind of array of images or ascii objects corresponding to life #
+// 
+// Display of letters
+//      One for each letter of the alphabet. etc. this could be done in html?
+//      on click, record that letter. let that button fade out
+//      run it against the current word
+//          if is in:
+//          if is not: -1 life
+// 
+// oh fuck i know
+// baseSet var
+// lives.baseSet = 6
+// letters.baseSet = ?? make disappeared letter buttons appear again?? fuck idk
