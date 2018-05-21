1e2067fffb7573a392309092851834289e1bcc98
helioboros <felinewraith@gmail.com>
2018-05-21 16:53:22 -0400

    removed stuff i thought was organizing but made it worse

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const createRandomWord = function () {
-    let testArray = ["one", "two", "three", "four", "five"]
-    let randomWord = (testArray[(Math.floor(Math.random() * testArray.length))])
-    let correctLettersArray = randomWord.split("")
-},
-    // - randomize array code or whatever. work on this later. dont hate yourself please
+let testArray = ["one", "two", "three", "four", "five"]
+let randomWord = (testArray[(Math.floor(Math.random() * testArray.length))])
+let correctLettersArray = randomWord.split("")
+let lives = 6
 
-    // createRandomWord = function () {
-    //     wordArrays = {
-    //         testArray: ["one", "two", "three", "four", "five"],
-    //     }
-    //     let chosenArray = this.wordArrays.testArray
-    //     let randomWord = (chosenArray[(Math.floor(Math.random() * chosenArray.length))])
-    // }
+console.log(correctLettersArray)
+
+// - randomize array code or whatever. work on this later. dont hate yourself please
+
+// createRandomWord = function () {
+//     wordArrays = {
+//         testArray: ["one", "two", "three", "four", "five"],
+//     }
+//     let chosenArray = this.wordArrays.testArray
+//     let randomWord = (chosenArray[(Math.floor(Math.random() * chosenArray.length))])
+// }
 
 
 // some kind of 'prepare game' function for when you press a button would be cool
@@ -25,46 +27,46 @@ const createRandomWord = function () {
 //      replace corresponding _ to match it with the corresponding letter. maybe like
 //      array and match the number values?
 
-const uInterface = {
-    createWordTemplate: function () {
-        for (let ii = 0; ii < (randomWord.length * 2 - 1); ii++) {
-            if (ii % 2 == 0) {
-                this.append("_")
-            } else {
-                this.append(" ")
-            }
+function createWordTemplate(){
+    for (let ii = 0; ii < (randomWord.length * 2 - 1); ii++) {
+        if (ii % 2 == 0) {
+            this.append("_")
+        } else {
+            this.append(" ")
         }
-    },
-
-    guessedLetters: [],
-
-    lives: 6,
-    lifeHit: function () {
-        this.lives--
-    },
-    lifeUpdate: function () {
-        $(h3).text(lives)
-    },
-
-
-    reset: function () {
-        this.lives = 6
-        createRandomWord.randomWord()
-        //update word display
-        //return missing letters
-    },
-
-    letters: function () {
-
     }
+}
+
+guessedLetters: [],
+
+function lifeHit() {
+    lives--
+}
+
+function lifeUpdate() {
+    $(h3).text(lives)
+}
+
+
+function reset() {
+    //reset lives, randomize word
+    this.lives = 6
+    let randomWord = (testArray[(Math.floor(Math.random() * testArray.length))])
+    let correctLettersArray = randomWord.split("")
+    //return missing letters
+}
+
+function letters() {
+
+}
 
     // Display of letters
     //      on click, record that button's letter. let that button fade out
     //      run it against:
-    // createRandomWord.correctLettersArray[]
+    //      correctLettersArray
     //          if is in:
     //          if is not: -1 life
-}
+
 const interactable = {
     //put the things that happen when users click buttons here
 }
@@ -75,5 +77,5 @@ $(() => {
     $(".button").click(function () {
         this.fadeOut()
     })
-    $("#reset").click(uInterface.reset)
+    $("#reset").click(reset)
 })
