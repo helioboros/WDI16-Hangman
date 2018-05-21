6124ab332934f0822f8f34a14e384ec9a9e75fdc
helioboros <felinewraith@gmail.com>
2018-05-21 15:32:17 -0400

    cleaning house

--- script.js
+++ script.js
@@ -1,44 +1,51 @@
 const Hangman = {
 
-    // big ole hangman object array. just testing for now. picks one out of the letters.
-    // later a function to randomize the array would be neat?
+    // big ole hangman object array. just testing for now. picks one.
     testArray: ["one", "two", "three", "four", "five"],
     randomWord: (testArray[(Math.floor(Math.random() * testArray.length))]),
-    //  random word display: something about making an array at randomWord.length?
-    guessedLetters: [],
 
-    createWordTemplate: function () {
-        for (let ii = 0, i < (randomWord.length * 2), i++) {
-            if (ii % 2 == 0) {
-                //current.append("_")
-            } else {
-                //current.append(" ")
-            }
-        }
-    },
-    // function for resetting
-    //      - Lives: 6
-    //      - Word: random from such and such array
-    //      - Word display: updated
-    //      - Letter display: filled out
-    //
-    // object for stuff having to do with lives?
-    //      Display: some kind of array of images or ascii objects corresponding to life #
-    lifeRel = {
-        lives: 6
-        lifeHit: function () {
-            this.lives--
-        }
-        lifeUpdate: function () {
-            $(h3).html(lives)
-        }
-    }
+    // - randomize array code or whatever. work on this later. dont hate yourself please
+
+    // createRandomWord = function () {
+    //     wordArrays = {
+    //         testArray: ["one", "two", "three", "four", "five"],
+    //     }
+    //     let chosenArray = this.wordArrays.testArray
+    //     let randomWord = (chosenArray[(Math.floor(Math.random() * chosenArray.length))])
+    // }
+
+    // some kind of 'prepare game board' function for when you press a button would be cool
+    // for now let it start up when the page loads
     //
     // Display for the chosen word
     //      fill it with " _ " for # of letters in the word
     //      replace corresponding _ to match it with the corresponding letter. maybe like
     //      array and match the number values?
-    // 
+    createWordTemplate: function () {
+        for (let ii = 0; ii < (randomWord.length * 2 - 1); ii++) {
+            if (ii % 2 == 0) {
+                this.append("_")
+            } else {
+                this.append(" ")
+            }
+        }
+    },
+
+    guessedLetters: [],
+
+    lives: 6,
+    lifeHit: function () {
+        this.lives--
+    },
+    lifeUpdate: function () {
+        $(h3).text(lives)
+    }
+    // function for resetting parameters
+    //      - Lives: 6
+    //      - Word: random from such and such array
+    //      - Word display: updated
+    //      - Letter display: filled out
+
     // Display of letters
     //      on click, record that button's letter. let that button fade out
     //      run it against the current word
