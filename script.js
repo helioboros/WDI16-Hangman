653c5a47e512b04df88bdbec1bac0c059dd48fb9
helioboros <felinewraith@gmail.com>
2018-05-22 09:54:03 -0400

    Display now shows a slot for every letter of the random word, and reset button works

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let testArray = ["one", "two", "three", "four", "five"]
 let randomWord = (testArray[(Math.floor(Math.random() * testArray.length))])
 let correctLettersArray = randomWord.split("")
 let lives = 6
+lifeUpdate()
+createWordTemplate()
 
 // - randomize array code or whatever. work on this later. dont hate yourself please
 
@@ -25,45 +27,44 @@ let lives = 6
 //      replace corresponding _ to match it with the corresponding letter. maybe like
 //      array and match the number values?
 
-function createWordTemplate(){
+function createWordTemplate() {
+    $("h3").empty()
     for (let ii = 0; ii < (randomWord.length * 2 - 1); ii++) {
         if (ii % 2 == 0) {
-            this.append("_")
+            $("h3").append("_")
         } else {
-            this.append(" ")
+            $("h3").append(" ")
         }
     }
 }
 
-guessedLetters: [],
-
 function lifeHit() {
     lives--
 }
-
 function lifeUpdate() {
-    $(h3).text(lives)
+    $("h2").text(lives + " lives remaining")
 }
 
 
 function reset() {
-    //reset lives, randomize word
-    this.lives = 6
-    let randomWord = (testArray[(Math.floor(Math.random() * testArray.length))])
-    let correctLettersArray = randomWord.split("")
-    //return missing letters
+    lives = 6
+    lifeUpdate()
+    randomWord = (testArray[(Math.floor(Math.random() * testArray.length))])
+    correctLettersArray = randomWord.split("")
+    createWordTemplate()
+    $(".button1").show()
 }
 
 function letters() {
 
 }
 
-    // Display of letters
-    //      on click, record that button's letter. let that button fade out
-    //      run it against:
-    //      correctLettersArray
-    //          if is in:
-    //          if is not: -1 life
+// Display of letters
+//      on click, record that button's letter. let that button fade out
+//      run it against:
+//      correctLettersArray
+//          if is in:
+//          if is not: -1 life
 
 const interactable = {
     //put the things that happen when users click buttons here
@@ -71,9 +72,8 @@ const interactable = {
 
 $(() => {
     //BUTTONS.
-    $("")
-    $(".button").click(function () {
-        this.fadeOut()
+    $(".button1").click(function () {
+        $(this).fadeOut("slow");
     })
     $("#reset").click(reset)
 })
