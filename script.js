812971009326b7b61f33d8f445f937f55a56ed12
helioboros <felinewraith@gmail.com>
2018-05-23 07:33:56 -0400

    testing subject randomizing

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-const testArray = ["ae", "aei", "aeio", "aeiou", "aeiouy"]
-let chosenArray = testArray
+const randomArray = [subjectAnimals, subjectPlants]
+const subjectAnimals = ["alligator", "ant", "bear", "bee", "bird", "camel", "cat", "cheetah", "chicken", "chimpanzee", "cow", "crocodile", "deer", "dog", "dolphin", "duck", "eagle", "elephant", "fish", "fly", "fox", "frog", "giraffe", "goat", "goldfish", "hamster", "hippopotamus", "horse", "kangaroo", "kitten", "lion", "lobster", "monkey", "octopus", "owl", "panda", "pig", "puppy", "rabbit", "rat", "scorpion", "seal", "shark", "sheep", "snail", "snake", "spider", "squirrel", "tiger", "turtle", "wolf", "zebra"]
+const subjectPlants = ["hydrangea", "hyacinth", "rose"]
+let chosenArray = [(Math.floor(Math.random() * randomArray.length))]
 let randomWord = (chosenArray[(Math.floor(Math.random() * chosenArray.length))])
 let lives = 6
 let completionProgress = randomWord.length
 let answerArray = []
 let guess
+let subjectName
 
 lifeUpdate()
 createWordTemplate()
+subjectDisplayUpdate()
+
+function subjectDisplayUpdate() {
+    if (chosenArray == subjectAnimals) {
+        subjectName = "Animals"
+    } else if (chosenArray == subjectPlants) {
+        subjectName = "Plants"
+    } else {subjectName = "There's a problem here."}
+    $("h1").text("Current topic: " + subjectName)
+}
 
 // Refreshes number of lives for when you lose one or reset the game
 function lifeUpdate() {
@@ -46,14 +59,15 @@ function choice(letter){
     }
 }
 
-// reset function
+// Resets everything and chooses a random word from the chosen array
 function reset() {
+    answerArray = []
     guess = ""
     lives = 6
     lifeUpdate()
-    $("h3").empty()
     randomWord = (chosenArray[(Math.floor(Math.random() * chosenArray.length))])
     completionProgress = randomWord.length
+    $("h3").empty()
     createWordTemplate()
     $(".button1").show()
 }
